9d81612e391777eb54f455605921f44fa34c6d1f
elishabista <elishabista111@gmail.com>
2024-05-07 13:29:00 +0545

    feat: started learning about wordpress plugin

---
+++ simpleform-plugin/simpleform-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package simpleFormPlugin
+ */
+/*
+ * Plugin Name: Simple Form Plugin
+ * Plugin URI: http://simpleformplugin.com
+ * Description: This is my first wordpress plugin.
+ * Version: 1.0.0
+ * Author: XYZ
+ */
+
+if( !defined('ABSPATH')){
+exit;
+}
